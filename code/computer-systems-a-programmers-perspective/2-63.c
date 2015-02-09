a7f39d89ab68c125373507c241cfc5317e228fc5
qianlv <qianlv7@qq.com>
2015-02-09 10:37:58 +0800

    code

--- code/computer-systems-a-programmers-perspective/2-63.c
+++ code/computer-systems-a-programmers-perspective/2-63.c
@@ -7,7 +7,7 @@
 //        Version:  1.0
 //        Created:  2014年12月29日 16时38分47秒 CST
 //       Revision:  none
-//       Compiler:  g++
+//       Compiler:  gcc
 // 
 //         Author:  肖守冬, qianlv@qq.com
 //        License:  Copyright © 2014 肖守冬. All Rights Reserved.
@@ -20,4 +20,18 @@
 int sra(int x, int k)
 {
     int xsrl = (unsigned) x >> k;
+    int tmp = ~0;
+    int tmp = tmp << (8 * sizeof(int) - k);
+    int judge = (~0) << ( 8 * sizeof(int) - 1);
+    return 
+}
+
+// 逻辑右移
+int srl(unsigned x, int k)
+{
+    unsigned xsra = (int) x >> k;
+    int tmp = ~0; // 0xFFFFFFFF
+    int tmp = tmp << (8 * sizeof(int) - k); // k = 3: 1110 0000 0000 0000 0000 0000 0000 0000
+    int tmp = ~tmp;  // k = 3: 0001 1111 1111 1111 1111 1111 1111 1111
+    return tmop & xsra;
 }
